01e64b393ef661249eaab8829a64fe4f60c41329
sidweb00 <siddharthgandhi005@gmail.com>
2022-03-11 16:07:24 +0530

    initialize project

---
+++ StoresApi/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Store(models.Model):
+    # add more relevant fields
+    name = models.CharField(max_length=50)
+
+    # change as per use
+    def __str__(self):
+        return self.name
---
+++ StoresApi/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import StoreSerializer
+
+from .models import Store
+
+# This is a sample working of API
+# create like this for other APIs
+# GET, POST, PUT OR DELETE
+
+
+@api_view(['GET'])
+def testApi(request):
+    return Response("Api Working!!")
